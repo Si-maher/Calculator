fdc3880032ba3c6980c3426b1446578cb98418a4
Simon Maher <simonmaher8307@hotmail.com>
2019-10-07 14:30:22 +0300

    created all consts for Javascript

---
+++ index.js
@@ -0,0 +1,4 @@
+const btns = document.querySelectorAll('.btns')
+const screen = document.querySelectorAll('.screen')
+const equalBtn = document.querySelectorAll('.btn-equal')
+const clearBtn = document.querySelectorAll('.btn-clear')
