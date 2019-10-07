440ecdf960eeeed9d7f2e905143ff09523547c9a
Simon Maher <simonmaher8307@hotmail.com>
2019-10-07 14:37:48 +0300

    added for loop for the buttons

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const btns = document.querySelectorAll('.btns')
 const screen = document.querySelectorAll('.screen')
 const equalBtn = document.querySelectorAll('.btn-equal')
 const clearBtn = document.querySelectorAll('.btn-clear')
+
+for(i = 0; i < btns.length; i ++) {
+    btns[i].addEventListener('click', function(){
+        let number = btns[i].getAttribute('data-num')
+        screen.value += number
+    })
+}
